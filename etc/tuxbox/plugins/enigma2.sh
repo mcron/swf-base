99e4f2f08b5b8074df52af625d7f48f7316d39d5
mcron <mcron@hotmail.de>
2013-11-04 16:54:04 +0100

    fix script

--- etc/tuxbox/plugins/enigma2.sh
+++ etc/tuxbox/plugins/enigma2.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 # start enigma2
-if [ -e /usr/bin/enigma2 ]; then
-	wget -q -O - http://127.0.0.1:80/control/message?popup="enigma2 will start..." > /dev/null
-	sleep 4
-	killall -9 neutrino
-	enigma2
-fi
-
+wget -q -O - http://127.0.0.1:80/control/message?popup="enigma2 will start..." > /dev/null
+sleep 5
+killall -9 neutrino
+init 3
 
 exit 0
